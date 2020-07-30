399057a93bc5dff503da258189a029f18466f37f
HanJin Park <phj9020@hotmail.com>
2020-07-30 17:56:06 +0900

    Update background.js

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -5,7 +5,7 @@ const IMG_NUMBER = 4;
 
 function paintImage(imageNumber){
     const image = new Image();
-    image.src = `../image/${imageNumber + 1}.jpg`  // +1을 하는 이유 : math.random 함수가 0을 줄 수 있기 때문이다 
+    image.src = `./image/${imageNumber + 1}.jpg`  // +1을 하는 이유 : math.random 함수가 0을 줄 수 있기 때문이다 
     image.classList.add("bgImage");
     body.prepend(image);
 }
